f8199142d6aec8a049bf4ffc3076d89f72cbf1df
suprakashnegi <suprakashnegi@gmail.com>
2026-05-13 12:16:19 +0530

    file sorting feature added successfuly

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import os
+import shutil
+
+
 
 def scan_folder():
+
     try:
-
         folder_path = input("Enter folder path: ")
-
 
         if not os.path.exists(folder_path):
             print("Folder does not exist.")
             return
 
-
         items = os.listdir(folder_path)
-
 
         if len(items) == 0:
             print("Folder is empty.")
             return
 
         print("\nFiles and Folders:\n")
-
 
         for item in items:
             print(item)
@@ -29,4 +28,66 @@ def scan_folder():
 
 
 
-scan_folder()
+def organize_files():
+
+    try:
+        folder_path = input("Enter folder path: ")
+
+        if not os.path.exists(folder_path):
+            print("Folder does not exist.")
+            return
+
+        file_types = {
+            "Images": [".jpg", ".png", ".jpeg"],
+            "Documents": [".pdf", ".txt", ".docx"],
+            "Music": [".mp3"],
+            "Videos": [".mp4"]
+        }
+
+        items = os.listdir(folder_path)
+
+        for item in items:
+
+            item_path = os.path.join(folder_path, item)
+
+            # Skip folders
+            if os.path.isdir(item_path):
+                continue
+
+            _, extension = os.path.splitext(item)
+
+            for folder_name, extensions in file_types.items():
+
+                if extension.lower() in extensions:
+
+                    target_folder = os.path.join(folder_path, folder_name)
+
+                    os.makedirs(target_folder, exist_ok=True)
+
+                    shutil.move(
+                        item_path,
+                        os.path.join(target_folder, item)
+                    )
+
+                    print(f"Moved: {item} -> {folder_name}")
+
+                    break
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+print("1. Scan Folder")
+print("2. Organize Files")
+
+choice = input("Enter choice: ")
+
+if choice == "1":
+    scan_folder()
+
+elif choice == "2":
+    organize_files()
+
+else:
+    print("Invalid choice")
