dc62c5d03d4f4110971e5c01a994f2fa07a4c5b2
suprakashnegi <suprakashnegi@gmail.com>
2026-05-14 20:28:25 +0530

    new feature delete and logs store added

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
 import shutil
+import logging
 
+
+# Log file setup
+logging.basicConfig(
+    filename="log.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
 
 
 def scan_folder():
@@ -10,12 +18,14 @@ def scan_folder():
 
         if not os.path.exists(folder_path):
             print("Folder does not exist.")
+            logging.info("Folder not found")
             return
 
         items = os.listdir(folder_path)
 
         if len(items) == 0:
             print("Folder is empty.")
+            logging.info("Empty folder scanned")
             return
 
         print("\nFiles and Folders:\n")
@@ -23,9 +33,11 @@ def scan_folder():
         for item in items:
             print(item)
 
+        logging.info("Folder scanned successfully")
+
     except Exception as e:
         print(f"Error: {e}")
-
+        logging.error(f"Error: {e}")
 
 
 def organize_files():
@@ -35,6 +47,7 @@ def organize_files():
 
         if not os.path.exists(folder_path):
             print("Folder does not exist.")
+            logging.info("Folder not found")
             return
 
         file_types = {
@@ -71,23 +84,71 @@ def organize_files():
 
                     print(f"Moved: {item} -> {folder_name}")
 
+                    logging.info(f"Moved file: {item}")
+
                     break
 
     except Exception as e:
         print(f"Error: {e}")
+        logging.error(f"Error: {e}")
 
 
+def delete_empty_folders():
+
+    try:
+        folder_path = input("Enter folder path: ")
+
+        if not os.path.exists(folder_path):
+            print("Folder does not exist.")
+            logging.info("Folder not found")
+            return
+
+        found = False
+
+        for root, dirs, files in os.walk(folder_path, topdown=False):
+
+            for folder in dirs:
+
+                folder_path_full = os.path.join(root, folder)
+
+                if len(os.listdir(folder_path_full)) == 0:
+
+                    os.rmdir(folder_path_full)
+
+                    print(f"Deleted: {folder_path_full}")
+
+                    logging.info(f"Deleted folder: {folder}")
+
+                    found = True
+
+        if not found:
+            print("No empty folders found.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        logging.error(f"Error: {e}")
+
+
+print("\nPython Automation Script\n")
 
 print("1. Scan Folder")
 print("2. Organize Files")
+print("3. Delete Empty Folders")
+print("4. Exit")
 
-choice = input("Enter choice: ")
+choice = input("\nEnter choice: ")
 
 if choice == "1":
     scan_folder()
 
 elif choice == "2":
     organize_files()
+
+elif choice == "3":
+    delete_empty_folders()
+
+elif choice == "4":
+    print("Program closed.")
 
 else:
     print("Invalid choice")
